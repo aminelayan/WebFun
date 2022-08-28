8a3901b08e21ccd0421a4249a2e5e25d32f4ecd8
Amin Elayan <ameen.m.elayyan@gmail.com>
2022-08-28 13:56:06 +0300

    Loop Challenge

---
+++ JS/Loop Challenge/Loop Challenge.js
@@ -0,0 +1,34 @@
+//Decreasing Multiples of 3
+for(let i=100;i>=0;i-=3){
+    console.log(i)
+}
+//Print the sequence
+for(let i=4;i>= -3.5;i-=1.5){
+    console.log(i)
+}
+//Sigma
+function sigma(){
+    var sum=0
+for(let i=0;i<= 100;i++){
+    sum=sum+i
+    console.log(sum)
+}
+}
+sigma()
+
+//Factorial
+
+function factorial(){
+var num=1
+for(let i=1;i<= 12;i++){
+    num=num*i
+    console.log(num)
+}
+}
+factorial()
+
+//Print odds 1-20
+for(i=1;i<=20;i++){
+    if(i%3==0)
+    console.log(i)
+}
