aa40dd3bf0a3b50646eb4d9ebf744855c82e92a9
Amin Elayan <ameen.m.elayyan@gmail.com>
2022-08-29 21:59:20 +0300

    update

--- JS/Dojo Pizzeria/pizza.js
+++ JS/Dojo Pizzeria/pizza.js
@@ -7,39 +7,15 @@ function pizzaOven(crusttype,saucetype,cheese,toppings) {
     pizza.toppings=toppings;
     return pizza;
 };
-var s1 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
-console.log(JSON.stringify(s1));
+var p1 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
+console.log(JSON.stringify(p1));
 
-function pizzaOven(crusttype,saucetype,cheese,toppings) {
-    var pizza ={};
-    pizza.crusttype=crusttype;
-    pizza.saucetype=saucetype;
-    pizza.cheese=cheese;
-    pizza.toppings=toppings;
-    return pizza;
-};
-var s2 = pizzaOven("hand tossed", "marinara", ["mozzarella","feta"] ,["mushrooms", "olives", "onions"]);
-console.log(JSON.stringify(s2));
+var p2 = pizzaOven("hand tossed", "marinara", ["mozzarella","feta"] ,["mushrooms", "olives", "onions"]);
+console.log(JSON.stringify(p2));
 
-function pizzaOven(crusttype,saucetype,cheese,toppings) {
-    var pizza ={};
-    pizza.crusttype=crusttype;
-    pizza.saucetype=saucetype;
-    pizza.cheese=cheese;
-    pizza.toppings=toppings;
-    return pizza;
-};
-var s3 = pizzaOven("italian crust", "marinara", ["mozzarella","emmental"], "pineapple");
-console.log(JSON.stringify(s3));
+var p3 = pizzaOven("italian crust", "marinara", ["mozzarella","emmental"], "pineapple");
+console.log(JSON.stringify(p3));
 
-function pizzaOven(crusttype,saucetype,cheese,toppings) {
-    var pizza ={};
-    pizza.crusttype=crusttype;
-    pizza.saucetype=saucetype;
-    pizza.cheese=cheese;
-    pizza.toppings=toppings;
-    return pizza;
-};
-var s4 = pizzaOven("thick crust", "ranch", ["mozzarella","emmental"], ["chicken","jalapeno","onions"]);
-console.log(JSON.stringify(s4));
+var p4 = pizzaOven("thick crust", "ranch", ["mozzarella","emmental"], ["chicken","jalapeno","onions"]);
+console.log(JSON.stringify(p4));
 
