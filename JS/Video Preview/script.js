4510e3f380bb753f6a3856bf3ab497f07baf9bb4
Amin Elayan <ameen.m.elayyan@gmail.com>
2022-08-29 15:02:32 +0300

    Video Preview

---
+++ JS/Video Preview/script.js
@@ -0,0 +1,31 @@
+document.getElementById("myVid").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+document.getElementById("myVid1").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid1").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+document.getElementById("myVid2").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid2").addEventListener("mouseleave", function() {
+	this.pause();
+});
+
+document.getElementById("myVid3").addEventListener("mouseover", function() {
+	this.play();
+});
+
+document.getElementById("myVid3").addEventListener("mouseleave", function() {
+this.pause();
+});
